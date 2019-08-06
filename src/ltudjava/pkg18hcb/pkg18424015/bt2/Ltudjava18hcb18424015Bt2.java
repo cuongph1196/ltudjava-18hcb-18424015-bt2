a42db78d8fbe9868a19b755f6edf08aae610633f
cuongph1196 <phancuong1196@gmail.com>
2019-08-06 23:34:46 +0700

    fist commit

---
+++ src/ltudjava/pkg18hcb/pkg18424015/bt2/Ltudjava18hcb18424015Bt2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ltudjava.pkg18hcb.pkg18424015.bt2;
+
+/**
+ *
+ * @author phanc
+ */
+public class Ltudjava18hcb18424015Bt2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
