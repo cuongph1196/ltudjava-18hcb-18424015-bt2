a623e7bd062bf2aece8bb3fb0fc46583e95eb359
cuongph1196 <phancuong1196@gmail.com>
2019-08-09 00:22:48 +0700

    setup hibernate and dao

---
+++ src/ltudjava/pkg18hcb/pkg18424015/bt2/DAO/SinhVienDAO.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ltudjava.pkg18hcb.pkg18424015.bt2.DAO;
+
+import connection.Controller;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pojo.Sinhvien;
+
+/**
+ *
+ * @author phanc
+ */
+public class SinhVienDAO {
+    public static List<Sinhvien> layDanhSachSinhVien() {
+        List<Sinhvien> ds = null;
+        Session session = Controller.getSessionFactory().openSession();
+        try {
+            String hql = "select sv from Sinhvien sv";
+            Query query = session.createQuery(hql);
+            ds = query.list();
+        } catch (HibernateException ex) {
+            //Log the exception
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return ds;
+    }
+    
+    public static Sinhvien layThongTinSinhVien(String maSinhVien) {
+        Sinhvien sv = null;
+        Session session = Controller.getSessionFactory().openSession();
+        try {
+            sv = (Sinhvien) session.get(Sinhvien.class, maSinhVien);
+        } catch (HibernateException ex) {
+            //Log the exception
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return sv;
+    }
+    
+    public static boolean themSinhVien(Sinhvien sv) {
+        Session session = Controller.getSessionFactory().openSession();
+        if (SinhVienDAO.layThongTinSinhVien(sv.getMssv())!=null) {
+        return false;
+        }
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(sv);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            //Log the exception
+            transaction.rollback();
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return true;
+    }
+    
+    public static boolean capNhatThongTinSinhVien(Sinhvien sv) {
+        Session session = Controller.getSessionFactory().openSession();
+        if (SinhVienDAO.layThongTinSinhVien(sv.getMssv()) == null) {
+        return false;
+        }
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(sv);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            //Log the exception
+            transaction.rollback();
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return true;
+    }
+    
+    public static boolean xoaSinhVien(String maSinhVien) {
+        Session session = Controller.getSessionFactory().openSession();
+        Sinhvien sv = SinhVienDAO.layThongTinSinhVien(maSinhVien);
+        if(sv==null){
+            return false;
+        }
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(sv);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            //Log the exception
+            transaction.rollback();
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return true;
+    }
+}
--- src/ltudjava/pkg18hcb/pkg18424015/bt2/Ltudjava18hcb18424015Bt2.java
+++ src/ltudjava/pkg18hcb/pkg18424015/bt2/Ltudjava18hcb18424015Bt2.java
@@ -5,6 +5,12 @@
  */
 package ltudjava.pkg18hcb.pkg18424015.bt2;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import ltudjava.pkg18hcb.pkg18424015.bt2.DAO.SinhVienDAO;
+import pojo.Sinhvien;
+
 /**
  *
  * @author phanc
@@ -16,6 +22,56 @@ public class Ltudjava18hcb18424015Bt2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+//        List<Sinhvien> ds = SinhVienDAO.layDanhSachSinhVien();
+//        for(int i=0; i<ds.size(); i++){
+//            Sinhvien sv=ds.get(i);
+//            System.out.println("MSSV: "+sv.getMssv());
+//            System.out.println("Họ và tên: "+sv.getHoTen());
+//            System.out.println("CMND: "+ sv.getCmnd());
+//        }
+        
+//        Sinhvien sv = SinhVienDAO.layThongTinSinhVien("SV0001");
+//        if(sv!= null){
+//            System.out.println("MSSV: " + sv.getMssv());
+//            System.out.println("Họ và tên: " + sv.getHoTen());
+//            System.out.println("CMND: " + sv.getCmnd());
+//        }else{
+//            System.out.println("Sinh viên 0312143 không tồn tại");
+//        }
+
+//            Sinhvien sv = new Sinhvien();
+//            sv.setMssv("0312171");
+//            sv.setHoTen("Tạ Tấn Thêm");
+//            sv.setCmnd("312548597");
+////            Calendar calendar = Calendar.getInstance();
+////            calendar.set(1985, 5, 18);
+////            Date d = calendar.getTime();
+////            sv.setNgaySinh(d);
+////            sv.setDiaChi("Vũng Liêm – Vĩnh Long");
+//            boolean kq = SinhVienDAO.themSinhVien(sv);
+//            if (kq == true) {
+//              System.out.println("Thêm thành công");
+//            } else {
+//              System.out.println("Thêm thất bại");
+//            }
+
+//            Sinhvien sv = SinhVienDAO.layThongTinSinhVien("0312171");
+//            if (sv != null) {
+//                sv.setHoTen("Tạ Tấn Thêm");
+//                sv.setCmnd("123456789");
+//                boolean kq = SinhVienDAO.capNhatThongTinSinhVien(sv);
+//                if (kq == true) {
+//                    System.out.println("Cập nhật thành công");
+//                } else {
+//                    System.out.println("Cập nhật thất bại");
+//                }
+//            }
+
+        boolean kq = SinhVienDAO.xoaSinhVien("0312171");
+        if (kq == true) {
+            System.out.println("Xóa thành công");
+        } else {
+            System.out.println("Xóa thất bại");
+        }
     }
-    
 }
